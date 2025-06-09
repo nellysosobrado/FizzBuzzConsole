368811eee3d3fb9d2a23c43a9f490def2828b256
Nelly <Nelly.sosobrado@gmail.com>
2025-06-09 08:05:08 +0200

    Fizzbuzz metod logic to return value

---
+++ FizzBuzzConsole/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConsole
+{
+    public class FizzBuzz
+    {
+        public string WriteFizzBuzz(long num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+    }
+}
