aa9902ef57ac86a168a1ee08ef97f44e06d95e6c
Nelly <Nelly.sosobrado@gmail.com>
2025-06-09 08:08:18 +0200

    Added two menu

--- FizzBuzzConsole/Program.cs
+++ FizzBuzzConsole/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int menuChoice = 0;
+
+            while (true)
+            {
+                menuChoice = ShowMenu();
+                if (menuChoice == 0)
+                    break;
+                Run(menuChoice);
+            }
+            static int ShowMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("Choose from the menu below");
+                Console.WriteLine("1: Input one number (int): ");
+                Console.WriteLine("2: Run FizzBuzz from 1-100");
+                Console.WriteLine("0: Exit");
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            static void Run(int menuChoice)
+            {
+                long num = 0;
+                var runFizzBuzz = new FizzBuzz();
+
+                if (menuChoice == 1)
+                {
+                    Console.WriteLine("Please choose a whole number: ");
+                    num = Convert.ToInt64(Console.ReadLine());
+                }
+
+                switch (menuChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Result");
+                        Console.WriteLine("======");
+                        Console.WriteLine(runFizzBuzz.WriteFizzBuzz(num));
+                        Console.WriteLine("Press any key to continue: ");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Results");
+                        Console.WriteLine("=======");
+
+                        for (int i = 1; i <= 100; i++)
+                        {
+                            Console.WriteLine(runFizzBuzz.WriteFizzBuzz(i));
+                        }
+                        Console.WriteLine("Press any key to continue: ");
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 }
